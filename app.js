843119a754daa1bf627b5e9d3fc51c4d01fb06b5
Barent Langwell <barent@Barents-MacBook-Pro.local>
2020-07-23 15:53:13 -0700

    [oop] Add GithubProfile class

--- app.js
+++ app.js
@@ -69,21 +69,25 @@ function NBAPlayer(name, team, threePointShooter) {
     this.name = name;
     this.team = team;
     this.threePointShooter = threePointShooter;
-
+    this.intro = function() {
+        console.log('hi my name is ' + this.name);
+    }
     // return this object
 
-}
+};
 
 let steph = new NBAPlayer('steph curry', 'warriors', true);
 console.log(steph);
 console.log(steph.name);
-
 
 function videoGames(name, genre, rating, score) {
     this.name = name;
     this.genre = genre;
     this.rating = rating;
     this.score = score;
+    this.breakdown = function() {
+        console.log('I rate ' + this.name + ' ' + score + ' out of 10');
+    }
 }
 
 let ds3 = new videoGames('Dark Souls 3', 'ARPG', 'M', 9);
@@ -94,3 +98,68 @@ console.log(hzd);
 
 let fc5 = new videoGames('Farcry 5', 'FPS', 'M', 9);
 console.log(fc5);
+
+steph.intro();
+ds3.breakdown();
+
+// class 
+class Car {
+    // constructor is what is passed into class
+    // constructor function
+    constructor(year, make, model, color) {
+        this.year = year;
+        this.make = make;
+        this.model = model;
+        this.color = color;
+    }
+
+    drive() {
+        console.log(this.make + ' go vroooom');
+    }
+    intro() {
+        console.log('This car is a ' + this.make + ' ' + this.model);
+    }
+}
+
+let tesla = new Car('2020', 'Tesla', 'Model 1', 'Black');
+
+tesla.drive();
+tesla.intro();
+
+
+
+class GithubProfile {
+    constructor(username, name, url) {
+        this.username = username;
+        this.name = name;
+        this.url = url;
+    }
+    
+    intro() {
+        console.log(`My name is ${this.name} and my username is @${this.username}`) // backtick string to include javscript
+    }
+} // convention indicates no semicolon after class definition
+
+// make my own class out of
+// muh github api
+// https://api.github.com/users/blangwell
+fetch('https://api.github.com/users/blangwell') //pass in url as a string
+.then(response => {
+    return response.json();
+})
+.then(data => {
+    console.log(data);
+    let githubUrl = data.url; 
+    console.log(githubUrl);
+    let githubUserName = data.login;
+    console.log(githubUserName);
+    let githubName = data.name;
+    console.log(githubName);
+
+    let barent = new GithubProfile(githubUserName, githubName, githubUrl);
+    console.log(barent);
+
+    barent.intro();
+})
+
+//grab login name url
